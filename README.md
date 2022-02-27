1d6e11ef952932b3f41bbe28c9e121aefe8617a0
Zachary Robinson <zrobins2@swarthmore.edu>
2022-02-27 12:46:26 -0500

    Fix and run autoformat

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 A simple Cloudflare Worker to forward webhook events from Cronitor to a Discord channel.
 
 Webhook URL should be set in a `DISCORD_WEBHOOK` secret.
-
--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-addEventListener("fetch", (event) => {
+addEventListener('fetch', event => {
   event.respondWith(
     handleRequest(event.request).catch(
-      (err) => new Response(err.stack, { status: 500 })
-    )
-  );
-});
+      err => new Response(err.stack, { status: 500 }),
+    ),
+  )
+})
 
 async function handleRequest(request) {
-  const { pathname } = new URL(request.url);
+  const { pathname } = new URL(request.url)
 
-  if (pathname.startsWith("/alert")) {
+  if (pathname.startsWith('/alert')) {
     const body = await request.json()
 
     const payload = {
-        "content": null,
-        "embeds": [
-          {
-            "title": body.monitor,
-            "description": body.description,
-            "url": "https://cronitor.io/app/monitors/" + body.id,
-            "color": 16016675,
-            "author": {
-              "name": body.type + ": " + body.rule
-            }
-          }
-        ]
-      }
-    await fetch(DISCORD_WEBHOOK, 
-    {
-    body: JSON.stringify(payload),
-    method: "POST",
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-    },
-  })
+      content: null,
+      embeds: [
+        {
+          title: body.monitor,
+          description: body.description,
+          url: 'https://cronitor.io/app/monitors/' + body.id,
+          color: 16016675,
+          author: {
+            name: body.type + ': ' + body.rule,
+          },
+        },
+      ],
+    }
+    await fetch(DISCORD_WEBHOOK, {
+      body: JSON.stringify(payload),
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    })
 
-    return new Response();
+    return new Response()
   }
 
-  return new Response("", {status: 404});
+  return new Response('', { status: 404 })
 }
